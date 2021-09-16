b3991ea31a800b77ae2ecb53cedff0a6e908354b
Johannes Illerhaus <johannes.illerhaus@mytum.de>
2021-09-16 14:00:44 +0200

    state 16.09.21

--- trainGPU/makeDataset.py
+++ trainGPU/makeDataset.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+# In[16]:
+
+
+import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-train_df = pd.read_csv("../input/g2net-gravitational-wave-detection/test_paths.csv")
-savedir = "../input/filtered"
+
+# In[17]:
+
+
+INPUT_DIR = "../input/g2net-gravitational-wave-detection"
+train_df = pd.read_csv(f"{INPUT_DIR}/training_labels_paths.csv")
+test_df = pd.read_csv(f"{INPUT_DIR}/test_paths.csv")
+savedir = "../input/filtered_train"
+
+
+# In[30]:
+
 
 import gwpy
 from gwpy.timeseries import TimeSeries
 from scipy.cluster.vq import whiten
 from gwpy.signal import filter_design
-from tqdm import tqdm
+from tqdm.notebook import tqdm
+import matplotlib.pyplot as plt
 
-def save_files(rg_n):
+def save_files(rg_n, df=train_df):
     filts=[]
     for j in range(3):
-        bp = filter_design.bandpass(50,250, 2048)
+        bp = filter_design.bandpass(50,550, 2048)
         if j == 2:
             notch_freqs = (50, 100, 150)
         else:
@@ -25,31 +41,31 @@ def save_files(rg_n):
         filts.append(zpk)
 
     for index in tqdm(rg_n):
-        path, ident = train_df.iloc[index][["path", "id"]]
+        path, ident = df.iloc[index][["path", "id"]]
         ts_data = np.load(path).astype(np.float32)
 
         for i in range(3):
             measurement = ts_data[i]
-            ts = TimeSeries(measurement)
-            ts.sample_rate=2048
+            ts = TimeSeries(measurement, sample_rate=2048)
+            ts = ts.whiten(0.5, 0.25)
 
             zpk = filts[i]
             hfilt = ts.filter(zpk, filtfilt=True)
-            hfilt = whiten(hfilt)
             ts_data[i] = np.array(hfilt)
 
         ts_data = np.transpose(ts_data, [1,0])
         ts_data /= abs(ts_data).max()
         np.save(f"../input/filtered/{ident}.npy", ts_data)
 
+
+
 from glob import glob
-parts = 32
+parts = 16
 number = train_df.shape[0]//parts
-print(number)
 for i in range(parts):
     rg_n = range(i*number,(i+1)*number)
     if glob(f"{savedir}/{train_df.iloc[rg_n[0]].id}.npy") != []:
         print(f"skipping {i}")
     else:
         print(f"doing {i}")
-        save_files(rg_n)
+        save_files(rg_n, train_df)
