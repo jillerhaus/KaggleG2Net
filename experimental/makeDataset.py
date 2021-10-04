4499e614909d63065c51eea75c7db53b0f45998e
Johannes Illerhaus <johannes.illerhaus@mytum.de>
2021-10-04 18:52:37 +0200

    version with all features used during competition but before cleanup

--- experimental/makeDataset.py
+++ experimental/makeDataset.py
@@ -4,12 +4,15 @@
 # In[5]:
 
 
-import tensorflow as tf
+
+import os
+os.environ["AVAILABLE_CUDA_DEVICES"] ="-1"
 import pandas as pd
 import numpy as np
 from glob import glob
 import multiprocessing as mp
 import os
+from scipy import signal
 
 # In[2]:
 
@@ -17,33 +20,65 @@ import os
 INPUT_DIR = "../input/g2net-gravitational-wave-detection"
 train_df = pd.read_csv(f"{INPUT_DIR}/training_labels_paths.csv")
 test_df = pd.read_csv(f"{INPUT_DIR}/test_paths.csv")
-savedir = "../input/whitened-longer-tfrec"
+savedir = "../input/like-synthetic-whitened-highpassed-tfrec"
 
 
-# In[3]:
+def bandpass(x, lf=20, hf=500, order=8, sr=2048):
+    sos = signal.butter(order, [lf,hf], btype="bandpass", output="sos", fs=sr)
+    normalization = np.sqrt((hf-lf) / (sr/2))
+    if x.ndim == 2:
+        for i in range(3):
+            x[i] = signal.sosfilt(sos,x[i]) * normalization
+    elif x.ndim == 3:
+        for i in range(x,shape[0]):
+            for j in range(3):
+                x[i,j] = signal.sosfilt(sos,x[i,j]) * normalization
+    return x
 
+
+def highpass(x, lf=20, order=8, sr=2048):
+    sos = signal.butter(order, lf, btype="highpass", output="sos", fs=sr)
+    normalization = np.sqrt((2048-lf) / (sr/2))
+    if x.ndim == 2:
+        for i in range(3):
+            x[i] = signal.sosfilt(sos,x[i]) * normalization
+    elif x.ndim == 3:
+        for i in range(x,shape[0]):
+            for j in range(3):
+                x[i,j] = signal.sosfilt(sos,x[i,j]) * normalization
+    return x
+
+def prepare_wave(wave):
+    wave = np.load(train_df.path[0])
+    normalized_waves = []
+    scaling = np.array([1.5e-20,1.5e-20,0.5e-20], dtype = np.float32)
+    for i in range(3):
+        normalized_wave= wave[i] / scaling[i]
+        normalized_waves.append(normalized_wave)
+    wave = np.stack(normalized_waves, axis=0)
+    return wave
 
 import gwpy
 from gwpy.timeseries import TimeSeries
-from scipy.cluster.vq import whiten
-from gwpy.signal import filter_design
+# from scipy.cluster.vq import whiten
+# from gwpy.signal import filter_design
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-
+# from scipy import signal
 
 def save_files(rg_n, df=train_df):
     all_data_x = np.zeros([len(rg_n),4096,3], dtype=np.float32)
     all_data_y = np.zeros(len(rg_n), dtype=np.uint8)
-    filts=[]
-    for j in range(3):
-        bp = filter_design.bandpass(50,550, 2048)
-        if j == 2:
-            notch_freqs = (50, 100, 150)
-        else:
-            notch_freqs = (60, 120, 180)
-        notches = [filter_design.notch(line, 2048) for line in notch_freqs]
-        zpk = filter_design.concatenate_zpks(bp, *notches)
-        filts.append(zpk)
+#     filts=[]
+    # for j in range(3):
+        # bp = filter_design.bandpass(50,550, 2048)
+#         if j == 2:
+#             notch_freqs = (50, 100, 150)
+#         else:
+#             notch_freqs = (60, 120, 180)
+#         notches = [filter_design.notch(line, 2048) for line in notch_freqs]
+#         zpk = filter_design.concatenate_zpks(bp, *notches)
+#         filts.append(zpk)
     print(rg_n)
     slc = df.iloc[rg_n]
     print(slc.head())
@@ -55,30 +90,35 @@ def save_files(rg_n, df=train_df):
         all_data_y[index] = target
         ts_data = np.load(pth)
 
+        #ts_data = prepare_wave(ts_data)
+
+        #ts_data = bandpass(ts_data)
+
         for i in range(3):
             measurement = ts_data[i]
-            ts = TimeSeries(measurement, sample_rate=2048)
+            ts = TimeSeries(measurement)
+            ts.sample_rate = 2048
 
 
-            zpk = filts[i]
-            # ts = ts.filter(zpk, filtfilt=True)
-            ts = ts.whiten(1.0, 0.25)
+#             zpk = filts[i]
+# #             ts = ts.filter(zpk, filtfilt=True)
+            ts = ts.whiten(0.5, 0.25)
             ts_data[i] = np.array(ts)
+        ts_data = highpass(ts_data)
 
         ts_data = np.transpose(ts_data, [1,0])
         ts_data /= abs(ts_data).max()
-        # ts_data = ts_data * -1
+#         ts_data = ts_data * -1
         ts_data = ts_data.astype(np.float32)
         all_data_x[index] = ts_data
 
 
     return all_data_x, all_data_y, ids
 
-
 # In[8]:
 
 
-
+import tensorflow as tf
 
 def make_tfrec_train(batches, batch_size, i):
 
